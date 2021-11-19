615bd72b4b11e018dd57ed61ad9cd3e2c0d7b986
CyberXpl <94687268+CyberXpl@users.noreply.github.com>
2021-11-19 18:52:14 +0700

    Add files via upload

--- Matematika.py
+++ Matematika.py
@@ -5,13 +5,13 @@ import os
 os.system('clear')
 
 os.system("figlet kalkulator")
-print("       BY MR.CyberHack")
-print('(+)=========================(+)')
+print("       BY  MR.CyberHack X ./01Salamander")
+print('(+)===============================(+)')
 print('(1). penambahan')
 print('(2). perngurangan')
 print('(3). perkalian')
 print('(4). pembagian')
-print('(+)=====================(+)')
+print('(+)==============================(+)')
 pilih = input('pilihan : ')
 if pilih == 1:
   print
@@ -31,15 +31,13 @@ elif pilih == 3:
   print
   angka_1 = input('angka ke1 : ')
   angka_2 = input('angka ke2 : ')
-  total = angka_1 × angka_2
+  total = angka_1 * angka_2
   print 'hasil',total
 
 elif pilih == 4:
   print
   angka_1 = input('angka ke1 : ')
   angka_2 = input('angka ke2 : ')
-  total = angka_1 ÷ angka_2
+  total = angka_1 / angka_2
   print 'hasil',total
-      
-
 
